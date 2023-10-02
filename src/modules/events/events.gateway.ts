4b98d3e1606c9521b9064dda70a43a4aff106b71
cabrera-evil <douglascabrera.dev@gmail.com>
2023-10-01 21:58:46 -0600

    Add: storage client.id

--- src/modules/events/events.gateway.ts
+++ src/modules/events/events.gateway.ts
@@ -15,13 +15,25 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  // emit to the current client that a user has connected
+  private connectedUsers: Set<string> = new Set();
+
+  // emit to the current client that a user has connected and send the updated list of connected users to all clients
   handleConnection(client: Socket) {
+    this.connectedUsers.add(client.id);
     client.emit('connected', { id: client.id });
+    this.emitConnectedUsers();
   }
 
-  // emit to all connected clients except the current one that a user has disconnected
+  // emit to all connected clients except the current one that a user has disconnected and send the updated list of connected users to all clients
   handleDisconnect(client: Socket) {
+    this.connectedUsers.delete(client.id);
     client.broadcast.emit('disconnected', { id: client.id });
+    this.emitConnectedUsers();
+  }
+
+  // Send the updated list of connected users to all clients
+  private emitConnectedUsers() {
+    const usersArray = Array.from(this.connectedUsers);
+    this.server.emit('connectedUsers', usersArray);
   }
 }
